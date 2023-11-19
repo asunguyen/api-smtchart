9b87a4f303cfa05f91e17fa5ce5aab1f01066f26
asunguyen <cntt510@gmail.com>
2023-11-20 05:07:11 +0700

    update

--- controllers/vimoController.js
+++ controllers/vimoController.js
@@ -83,6 +83,195 @@ const vimoController = {
         }catch(err) {
             res.json({code: 500, error: err});
         }
+    }, 
+    getHtml: async(req, res) => {
+        try {
+            const url = req.query.url;
+            const response = await axios.get(url);
+            res.json({code: 200, data: response.data})
+        }catch (err) {
+            res.json({code : 500, error: err});
+        }
+    },
+    getDataCafef: async(req, res) => {
+        try {
+            const url = "https://msh-appdata.cafef.vn/rest-api/api/v1/OverviewOrgnizaztion/0/20231119/15?symbol=VNINDEX";
+            const response = await axios.get(url);
+            console.log(response.data);
+        }catch(err) {
+            res.json({code: 500, error: err});
+        }
+    },
+    tcttTheGioi: async (req, res) => {
+        try {
+            const type = req.query.type || "";
+            const url = "https://s.cafef.vn/ajax/mobile/smart/ajaxchisothegioi.ashx?type="+type;
+            const response = await axios.get(url);
+            res.json({code: 200, data: response.data.Data});
+        }catch(err) {
+            res.json({code: 500, error: err});
+        }
+    },
+    tcttVietNam: async (req, res) => {
+        try {
+            const typeChart = req.query.typeChart || "";
+            const index = req.query.index || "1;2;9;11;12";
+            const type = req.query.type || "market";
+            const floorCode = req.query.floorCode || "1";
+            if (typeChart && typeChart.length > 0) {
+                const urlTime = `https://s.cafef.vn/Ajax/Mobile/Smart/AjaxChartDataCenter.ashx?floorCode=${floorCode}&typeChart=${typeChart}`;
+                const responseTime = await axios.get(urlTime);
+                res.json({code: 200, data: responseTime.data.Data});
+            } else {
+                const url = `https://s.cafef.vn/Ajax/PageNew/RealtimeChartHeader.ashx?index=${index}&type=${type}`;
+                const response = await axios.get(url);
+                res.json({code: 200, data: response.data});
+            }
+        } catch (err) {
+            res.json({code: 500, error: err});
+        }
+    },
+    top10CoPhieu: async(req, res) => {
+        try {
+            const centerID = req.query.centerID || "HOSE";
+            const type = req.query.type || "UP";
+            const url = `https://s.cafef.vn/Ajax/Mobile/Smart/AjaxTop10CP.ashx?centerID=${centerID}&type=${type}`;
+            const response = await axios.get(url);
+            res.json({code: 200, data: response.data.Data});
+        }catch(err) {
+            res.json({code: 500, error: err});
+        }
+    },
+    dinhGiaIndex: async (req, res) => {
+        try {
+            const url = `https://s.cafef.vn/Ajax/PageNew/FinanceData/GetDataChartPE.ashx`;
+            const response = await axios.get(url);
+            res.json({code: 200, data: response.data.Data});
+        }catch(err) {
+            res.json({code: 500, error: err});
+        }
+    },
+    khoiNgoai: async(req, res) => {
+        try {
+            const type = req.query.type || "buy";
+            const url = `https://s.cafef.vn/ajax/mobile/smart/ajaxkhoingoai.ashx?type=${type}`;
+            const response = await axios.get(url);
+            res.json({code: 200, data: response.data.Data});
+        }catch(err) {
+            res.json({code: 500, error: err});
+        }
+    },
+    nhomDanDatThiTruong: async(req, res) => {
+        try {
+            const centerId = req.query.centerId || 1;
+            const url = `https://msh-appdata.cafef.vn/rest-api/api/v1/MarketLeaderGroup?centerId=${centerId}&take=10`;
+            const response = await axios.get(url);
+            res.json({code: 200, data: response.data.data});
+        }catch(err) {
+            res.json({code: 500, error: err});
+        }
+    },
+    top10TruyCap: async(req, res) => {
+        try {
+            const TypeGA = req.query.TypeGA || 7;
+            const url = `https://s.cafef.vn/Ajax/Mobile/Smart/AjaxTop10TraCuu.ashx?TypeGA=${TypeGA}`;
+            const response = await axios.get(url);
+            res.json({code: 200, data: response.data.Data});
+        }catch(err) {
+            res.json({code: 500, error: err});
+        }
+    },
+    dienBienGiaoDichKhoiNgoai: async(req, res) => {
+        try {
+            const dataDate = new Date();
+            const time = dataDate.getFullYear() + "" + (dataDate.getMonth() + 1) + "" + dataDate.getDate();
+            const symbol = req.query.symbol || "VNINDEX";
+            const url = `https://msh-appdata.cafef.vn/rest-api/api/v1/OverviewOrgnizaztion/0/${time}/15?symbol=${symbol}`;
+            const response = await axios.get(url);
+            res.json({code: 200, data: response.data});
+        }catch(err) {
+            res.json({code: 500, error: err});
+        }
+    },
+    thanhKhoanThiTruong: async(req, res) => {
+        try {
+            const symbol = req.query.symbol || "HOSE"
+            const url = `https://msh-appdata.cafef.vn/rest-api/api/v1/Liquidity/${symbol}`;
+            const response = await axios.get(url);
+            res.json({code: 200, data: response.data});
+        }catch(err) {
+            res.json({code: 500, error: err});
+        }
+    },
+    dataHangHoa: async(req, res) => {
+        try {
+            const type = req.query.type || 1;
+            const url = `https://s.cafef.vn/ajax/mobile/smart/ajaxhanghoa.ashx?type=${type}`;
+            const response = await axios.get(url);
+            res.json({code: 200, data: response.data.Data});
+        }catch(err) {
+            res.json({code: 500, error: err});
+        }
+    },
+    giaodichTuDoanh: async(req, res) => {
+        try {
+            const symbol = req.query.symbol || "VNINDEX";
+            const dataDate = new Date();
+            const time = dataDate.getFullYear() + "" + (dataDate.getMonth() + 1) + "" + dataDate.getDate();
+            const url = `https://s.cafef.vn/Ajax/PageNew/DataHistory/PriceHistory.ashx?Symbol=${symbol}&StartDate=&EndDate=&PageIndex=1&PageSize=20`;
+            const response = await axios.get(url);
+            const urlOver = `https://msh-appdata.cafef.vn/rest-api/api/v1/OverviewOrgnizaztion/1/${time}/20?symbol=${symbol}`
+            const responseOver = await axios.get(urlOver);
+            const dataRes = {
+                priceHistory: response.data.Data,
+                overView: responseOver.data
+            }
+            res.json({code: 200, data: dataRes});
+        }catch(err) {
+            res.json({code: 500, error: err});
+        }
+    },
+    topTuDoanh: async(req, res) => {
+        try {
+            const type = req.query.type || "BUYVALUE";
+            const url = `https://s.cafef.vn/ajax/mobile/smart/ajaxgiaodichtudoanh.ashx?type=${type}`;
+            const response = await axios.get(url);
+            res.json({code: 200, data: response.data.Data});
+        }catch(err) {
+            res.json({code: 500, error: err});
+        }
+    },
+    fDoRongThiTruong: async(req, res) => {
+        try {
+            const centerID = req.query.centerID || "HOSE";
+            const url = `https://s.cafef.vn/ajax/mobile/smart/ajaxdorongthitruong.ashx?centerID=${centerID}`;
+            const response = await axios.get(url);
+            res.json({code: 200, data: response.data.Data});
+        }catch(err) {
+            res.json({code: 500, error: err});
+        }
+    },
+    bandoThitruong: async(req, res) => {
+        try {
+            const type = req.query.type || 1;
+            const category = req.query.category || 0;
+            const centerId = req.query.centerId || 1;
+            const url = `https://s.cafef.vn/ajax/mobile/smart/ajaxbandothitruong.ashx?type=${type}&category=${category}&centerId=${centerId}`;
+            const response = await axios.get(url);
+            res.json({code: 200, data: response.data.Data});
+        }catch(err) {
+            res.json({code: 500, error: err});
+        }
+    },
+    thitruongNgoaiHoi: async(req, res)=> {
+        try {
+            const url = `https://s.cafef.vn/ajax/mobile/smart/ajaxtygia.ashx`;
+            const response = await axios.get(url);
+            res.json({code: 200, data: response.data.Data});
+        }catch(err) {
+            res.json({code: 500, error: err});
+        }
     }
+    
 }
 module.exports = vimoController;
--- routers/vimoRouter.js
+++ routers/vimoRouter.js
@@ -8,5 +8,23 @@ router.get("/top-10", vimoController.topTen);
 router.get("/dinh-gia", vimoController.dinhGia);
 router.get("/do-rong-thi-truong", vimoController.doRongThiTruong);
 router.post("/viet-stock/news-new-update-paging", vimoController.newsNewUpdatePaging);
-router.get("/viet-stock/report-data-by-display", vimoController.reportdatabydisplay)
+router.get("/viet-stock/report-data-by-display", vimoController.reportdatabydisplay);
+router.get("/get-html", vimoController.getHtml);
+
+router.get("/get-cafef", vimoController.getDataCafef);
+router.get("/chiso-thegioi", vimoController.tcttTheGioi);
+router.get("/chiso-index-vn", vimoController.tcttVietNam);
+router.get("/top-10-cophieu", vimoController.top10CoPhieu);
+router.get("/dinh-gia-index", vimoController.dinhGiaIndex);
+router.get("/khoi-ngoai", vimoController.khoiNgoai);
+router.get("/dan-dat-thi-truong", vimoController.nhomDanDatThiTruong);
+router.get("/top-10-truycap", vimoController.top10TruyCap);
+router.get("/dienbien-khoingoai", vimoController.dienBienGiaoDichKhoiNgoai);
+router.get("/thanhkhoan-thitruong", vimoController.thanhKhoanThiTruong);
+router.get("/hang-hoa", vimoController.dataHangHoa);
+router.get("/giaodich-tudoanh", vimoController.giaodichTuDoanh);
+router.get("/top-tudoanh", vimoController.topTuDoanh);
+router.get("/dorong-thitruong", vimoController.fDoRongThiTruong);
+router.get("/bando-thitruong", vimoController.bandoThitruong);
+router.get("/thitruong-ngoaihoi",vimoController.thitruongNgoaiHoi);
 module.exports = router;
